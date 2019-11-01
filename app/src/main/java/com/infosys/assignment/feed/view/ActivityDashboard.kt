3fc406a3bfe834be21bcdd20c3f4b1f1912b37ed
vinu <vinodtarle@gmail.com>
2019-11-02 03:16:43 +0530

    Add feed activity

---
+++ app/src/main/java/com/infosys/assignment/feed/view/ActivityDashboard.kt
@@ -0,0 +1,62 @@
+package com.infosys.assignment.feed.view
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.infosys.assignment.R
+import com.infosys.assignment.databinding.ActivityDashboardBinding
+import com.infosys.assignment.feed.adapter.AdapterFeed
+import com.infosys.assignment.feed.model.Feed
+import com.infosys.assignment.feed.viewmodel.ViewModelFeed
+
+class ActivityDashboard : AppCompatActivity() {
+    private lateinit var context: Context
+    private lateinit var binding: ActivityDashboardBinding
+    private lateinit var adapter: AdapterFeed
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+        getFeed()
+    }
+
+    private fun init() {
+        this.context = this
+        this.binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard)
+
+        // adapter init
+        this.adapter = AdapterFeed(context)
+        this.binding.rvFeeds.layoutManager = LinearLayoutManager(this.context)
+        this.binding.rvFeeds.setHasFixedSize(true)
+        this.binding.rvFeeds.adapter = adapter
+
+        // set onRefresh listener
+        this.binding.swipeRefresh.setOnRefreshListener { getFeed() }
+    }
+
+    private fun getFeed() {
+        ViewModelProviders.of(this)
+            .get(ViewModelFeed::class.java)
+            .getFeed()
+            .observe(this, Observer {
+                if (it.throwable != null) {
+                    bindFeed(it.rows!!)
+                } else {
+                    Toast.makeText(
+                        this.context,
+                        "Something went wrong, Try again later!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+    }
+
+    private fun bindFeed(feeds: List<Feed>) {
+        this.adapter.setData(feeds = feeds)
+    }
+}
--- app/src/test/java/com/infosys/assignment/ExampleUnitTest.kt
+++ app/src/test/java/com/infosys/assignment/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.infosys.assignment
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
